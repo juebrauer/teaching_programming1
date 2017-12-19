f85a00f89dcfa22d1532c68aa3f7835f1fee2809
juebrauer <juergen.brauer@hs-kempten.de>
2017-12-19 18:37:39 +0100

    New code from Tuesday, 19 December 2017: i) walking cycle, ii) base class Subject for class "enemy" and "student"

--- SuperStudent/SuperStudent/superstudent.cpp
+++ SuperStudent/SuperStudent/superstudent.cpp
@@ -4,6 +4,11 @@
 #include "opencv2/opencv.hpp"
 
 using namespace cv;
+using namespace std;
+
+#define NrWalkCycleImages 10
+
+enum WalkingDirections {left, right};
 
 struct coord2D
 {
@@ -19,18 +24,99 @@ struct coord2D
 enum damagetype {killing, energyreduction};
 
 
-class Enemy
+void overlayImage(const cv::Mat &background, const cv::Mat &foreground,
+    cv::Mat &output, cv::Point2i location)
+{
+    background.copyTo(output);
+
+
+    // start at the row indicated by location, or at row 0 if location.y is negative.
+    for (int y = std::max(location.y, 0); y < background.rows; ++y)
+    {
+        int fY = y - location.y; // because of the translation
+
+                                 // we are done of we have processed all rows of the foreground image.
+        if (fY >= foreground.rows)
+            break;
+
+        // start at the column indicated by location, 
+
+        // or at column 0 if location.x is negative.
+        for (int x = std::max(location.x, 0); x < background.cols; ++x)
+        {
+            int fX = x - location.x; // because of the translation.
+
+                                     // we are done with this row if the column is outside of the foreground image.
+            if (fX >= foreground.cols)
+                break;
+
+            // determine the opacity of the foregrond pixel, using its fourth (alpha) channel.
+            double opacity =
+                ((double)foreground.data[fY * foreground.step + fX * foreground.channels() + 3])
+
+                / 255.;
+
+
+            // and now combine the background and foreground pixel, using the opacity, 
+
+            // but only if opacity > 0.
+            for (int c = 0; opacity > 0 && c < output.channels(); ++c)
+            {
+                unsigned char foregroundPx =
+                    foreground.data[fY * foreground.step + fX * foreground.channels() + c];
+                unsigned char backgroundPx =
+                    background.data[y * background.step + x * background.channels() + c];
+                output.data[y*output.step + output.channels()*x + c] =
+                    backgroundPx * (1. - opacity) + foregroundPx * opacity;
+            }
+        }
+    }
+}
+
+
+class Subject
+{
+
+    public:
+    
+    Subject(string prefix)
+    {
+        float rfac = 0.25f;
+        for (int i = 1; i <= NrWalkCycleImages; i++)
+        {
+            char fname[500];
+            sprintf_s(fname, sizeof(fname), "pics/%s (%d).png",
+                      prefix.c_str(),
+                      i);
+
+            img[i - 1] = imread(fname, IMREAD_UNCHANGED);
+            printf("Nr of channels for image #%d is %d\n",
+                i, img[i - 1].channels());
+
+            resize(img[i - 1], img[i - 1],
+                Size((int)(img[i - 1].cols*rfac),
+                (int)(img[i - 1].rows*rfac)));
+        }
+    }
+
+    protected:
+
+    struct coord2D              position;
+    Mat                         img[NrWalkCycleImages];
+
+};
+
+
+class Enemy : public Subject
 {
     private:
     
-        struct coord2D position;
-
         enum damagetype damageeffect;
 
     public:
 
         // class constructor
-        Enemy()
+        Enemy() : Subject("robot/Run")
         {            
             printf("New enemy was generated.\n");
             position.x = 0;
@@ -62,17 +148,17 @@ class Enemy
 };
 
 
-class student
+class student : public Subject
 {
 public:
 
-    student()
-    {
-        img = imread("pics/student.png");
-        float rfac = 0.25f;
-        resize(img, img, Size((int)(img.cols*rfac), (int)(img.rows*rfac)));
+    student() : Subject("santaclaus/Walk")
+    {  
         position.x = 50;
         position.y = FLOOR_HEIGHT;
+
+        WalkCycleImgNr = 0;
+        WalkDir = WalkingDirections::right;
     }
 
     void update()
@@ -81,24 +167,47 @@ public:
         switch (c)
         {
             case 2424832: // cursor left
-                position.x += -5;
+                position.x += -5;              
+                WalkDir = WalkingDirections::left;
                 break;
             case 2555904: // cursor right
-                position.x += +5;
+                position.x += +5;       
+                WalkDir = WalkingDirections::right;
                 break;
         }
+
+        WalkCycleImgNr++;
+
+        /*
+        if (WalkCycleImgNr == -1)
+            WalkCycleImgNr = NrWalkCycleImages - 1;
+        */
+
+        if (WalkCycleImgNr == NrWalkCycleImages)
+            WalkCycleImgNr = 0;
     }
 
     void draw_into_image(Mat world)
     {
-        Mat dst_roi = world(Rect(position.x, position.y, img.cols, img.rows));
-        img.copyTo(dst_roi);
+        //Mat dst_roi = world(Rect(position.x, position.y, img[0].cols, img[0].rows));
+        //img[0].copyTo(dst_roi);
+
+        Mat sprite;
+        if (WalkDir == WalkingDirections::right)
+            sprite = img[WalkCycleImgNr];
+        else if (WalkDir == WalkingDirections::left)
+        {
+            cv::flip(img[WalkCycleImgNr], sprite, 1);
+        }
+
+
+        overlayImage(world, sprite, world, Point2i(position.x, position.y));
     }
 
 private:
-
-    struct coord2D position;
-    Mat            img;
+    
+    int                         WalkCycleImgNr;
+    enum WalkingDirections      WalkDir;
 
 };
 
@@ -108,16 +217,16 @@ int main()
 {
    
     Mat world(WORLD_WIDTH, WORLD_HEIGHT, CV_8UC3);
-    student s1;
+    student* s1 = new student();
 
     while (1)
     {
         // set complete world image to "white"
         world = Scalar(255, 255, 255);
 
-        s1.update();
+        s1->update();
 
-        s1.draw_into_image(world);
+        s1->draw_into_image(world);
 
         imshow("world", world);
 
